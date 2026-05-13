d1e4d1dd19f4c0e39cd2416866deb67783ba8a4c
j <Javi.guzmanm@duocuc.cl>
2026-05-13 10:46:14 -0400

    Added all pom files neccesary to mscv and carpets needed to make it

---
+++ Tournament-service/src/main/java/com/Esport_manager/Tournament_service/TournamentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Esport_manager.Tournament_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentServiceApplication.class, args);
+	}
+
+}
